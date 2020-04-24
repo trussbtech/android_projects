277576787e45602983faf155c020763e99929f4d
Tyrone Russ <tyronerussaws.@gmail.com>
2020-04-24 01:52:26 -0700

    second commit

---
+++ 1_config.groovy
@@ -0,0 +1,15 @@
+
+import jenkins.model.*;
+import java.util.logging.Logger;
+
+Logger logger = Logger.getLogger("")
+
+logger.info "Executing init script"
+
+def instance = Jenkins.instance
+instance.setDisableRememberMe(true)
+instance.setSystemMessage('Automating Jenkins with Groovy')
+instance.save()
+
+logger.info "Init script complete"
+© 2020 GitHub, Inc.
